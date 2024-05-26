55656329a548f379bd6a6bc68e7d6eb679f61310
yorliabdulai <yorliabdulai1@gmail.com>
2024-05-26 18:47:53 +0000

    Created  default data

---
+++ src/components/accordion/data.js
@@ -0,0 +1,29 @@
+const Data = [
+    {
+        id: '1',
+        question: "What is HTML?",
+        answer: "HTML stands for HyperText Markup Language. It is the standard markup language for creating web pages."
+    },
+    {
+        id: '2',
+        question: "What is CSS?",
+        answer: "CSS stands for Cascading Style Sheets. It is a style sheet language used for describing the look and formatting of a document written in HTML."
+    },
+    {
+        id: '3',
+        question: "What are the different types of CSS selectors?",
+        answer: "CSS selectors are used to select and style specific elements on a web page. Some common types of CSS selectors include element selectors, class selectors, ID selectors, attribute selectors, and pseudo-class selectors."
+    },
+    {
+        id: '4',
+        question: "What is the box model in CSS?",
+        answer: "The CSS box model is a design concept that describes how elements on a web page are rendered. It consists of the content area, padding, border, and margin of an element."
+    },
+    {
+        id: '5',
+        question: "What is the difference between inline and block elements in HTML?",
+        answer: "Inline elements do not start on a new line and only take up as much width as necessary, while block elements start on a new line and take up the full width available."
+    }
+];
+
+export default Data;
