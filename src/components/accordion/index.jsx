c007c8f8971754dcb7562591bcae31b8df11ba2b
yorliabdulai <yorliabdulai1@gmail.com>
2024-05-27 18:37:21 +0000

    Update import statement for styles.css in Accordion component

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';  
 import data from './data'; 
-import './style.css';
+import './styles.css';
 export default function Accordion() { 
  const [selected, setSelected] = useState(null);
  function handleSingleSelection(getCurrentId){
