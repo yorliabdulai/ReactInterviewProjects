77ab906a27bc8cedee6336217f9830b922a21a29
yorliabdulai <yorliabdulai1@gmail.com>
2024-05-27 18:25:42 +0000

    Add single selection functionality to accordion component

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -2,18 +2,27 @@ import React, {useState} from 'react';
 import data from './data'; 
 export default function Accordion() { 
  const [selected, setSelected] = useState(null);
+ function handleSingleSelection(getCurrentId){
+   setSelected(getCurrentId);
+ }
   return <div className='wrapper'>
     <div className='accordion'>
       {
         data && data.length > 0 ? 
         data.map(dataItem => <div className='item'>
-          <div className='title'>
+          <div onClick={()=> handleSingleSelection(dataItem.id)} className='title'>
             <h3>{dataItem.question}</h3>
             <span>+</span>
           </div>
+          {
+           selected === dataItem.id ? <div className='content'>
+            {dataItem.answer}
+            </div> : null
+          }
         </div>) 
         :<div>No data found!</div> 
       }
+     
     </div>
   </div>
 
