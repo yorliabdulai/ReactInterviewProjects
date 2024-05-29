53176de544f55f12ff413d7bf080a77286ec71c6
yorliabdulai <yorliabdulai1@gmail.com>
2024-05-29 19:38:01 +0000

    Add multi-selection functionality to Accordion component

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -3,16 +3,25 @@ import data from './data';
 import './styles.css';
 export default function Accordion() { 
  const [selected, setSelected] = useState(null);
+ const [enableMultiSelection, setEnableMultiSelection] = useState(false)
+ const [multiple, setMultiple] = useState([])
+
+
  function handleSingleSelection(getCurrentId){
    setSelected(getCurrentId === selected ? null : getCurrentId);
  }
+ function handleMultiselection(getCurrentId){
+
+ }
+
   return <div className='wrapper'>
-    <button>Enable Multi Selection</button>
+    <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>Enable Multi Selection</button>
     <div className='accordion'>
       {
         data && data.length > 0 ? 
         data.map(dataItem => <div className='item'>
-          <div onClick={()=> handleSingleSelection(dataItem.id)} className='title'>
+          <div onClick={enableMultiSelection? handleMultiselection(dataItem.id)
+            :()=> handleSingleSelection(dataItem.id)} className='title'>
             <h3>{dataItem.question}</h3>
             <span>+</span>
           </div>
