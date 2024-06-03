76b4ba1910ae4bbb2677a3b1d2c8b76b5e2d9a0d
yorliabdulai <yorliabdulai1@gmail.com>
2024-06-03 12:08:43 +0000

    Refactor RandomColor component to include background color and fix styling

--- src/components/random-color/index.jsx
+++ src/components/random-color/index.jsx
@@ -1,13 +1,19 @@
-
+import { useState } from 'react';
 
 
 
 export default function RandomColor() {
   const [typeOfColor, setTypeOfColor] = useState('hex'); 
   const [color, setColor] = useState('#000000')
-  return <div style>
-    <button>Create HEX Color</button>
-    <button>Create RGB Color</button>
-    <button>Generate Random Color</button>
-  </div>
+  return (
+    <div style={{
+      height: '100vh',
+      width: '100vw',
+      backgroundColor: color,
+    }}>
+      <button>Create HEX Color</button>
+      <button>Create RGB Color</button>
+      <button>Generate Random Color</button>
+    </div>
+  );
 }   
