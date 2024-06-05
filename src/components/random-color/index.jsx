3a809d9491853ef95a3ea880b132cd24bd146c48
yorliabdulai <yorliabdulai1@gmail.com>
2024-06-05 17:58:35 +0000

    Add function to create random colors

--- src/components/random-color/index.jsx
+++ src/components/random-color/index.jsx
@@ -5,6 +5,16 @@ import { useState } from 'react';
 export default function RandomColor() {
   const [typeOfColor, setTypeOfColor] = useState('hex'); 
   const [color, setColor] = useState('#000000')
+
+  function handleCreateRandomColor() {
+    if (typeOfColor === 'hex') {
+      const randomColor = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+      setColor(randomColor);
+    } else {
+      const randomColor = `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`;
+      setColor(randomColor);
+    }
+  }
   return (
     <div style={{
       height: '100vh',
