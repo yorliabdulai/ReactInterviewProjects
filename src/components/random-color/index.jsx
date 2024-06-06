48014c76a563745cbf7fc1b02eaa41a85ed86b9d
yorliabdulai <yorliabdulai1@gmail.com>
2024-06-06 19:18:10 +0000

    Update color display based on typeOfColor value

--- src/components/random-color/index.jsx
+++ src/components/random-color/index.jsx
@@ -32,7 +32,7 @@ export default function RandomColor() {
       fontSize: '60px',
       marginTop: '50px',
      }}>
-      <h1>{typeOfColor}</h1>
+      <h1>{typeOfColor === 'rgb'? 'RGB Color' : 'HEX Color'}</h1>
      <h1>{color}</h1>
      </div>
     </div>
